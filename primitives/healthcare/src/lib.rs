bbf6cebcd3f767c0fd38a989634f6a325319df0c
linerocks <tirtadwipa.manunggal@gmail.com>
2022-12-25 18:19:17 +0700

    add aria primitives

---
+++ primitives/healthcare/src/lib.rs
@@ -0,0 +1,64 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
+use sp_std::cmp::{Eq, PartialEq};
+
+// Enum declaration for User role.
+#[derive(Clone, Encode, Decode, PartialEq, Eq, TypeInfo)]
+pub enum Role {
+  Custodian = 1,
+  Virtual = 2,
+  Patient = 3,
+  FamilyHealth = 4,
+  MedicalProfessional = 5,
+  NonMedicalPersonnel = 6,
+  HealthFacility = 7,
+  Aggregator = 8,
+  Researcher = 9,
+  Enterprise = 10,
+  EnterpriseEmployee = 11,
+}
+
+#[derive(Copy, Clone, Encode, Decode, PartialEq, Eq, TypeInfo)]
+pub enum RecordFormat {
+  Hl7v2 = 0,
+  Dicom = 1,
+  FhirDstu1 = 2,
+  FhirDstu2_1 = 3,
+  FhirDstu3 = 4,
+  FhirStu3 = 5,
+  FhirR4 = 6,
+  FhirR5 = 7,
+}
+
+#[derive(Copy, Clone, Encode, Decode, PartialEq, Eq, TypeInfo)]
+pub enum TherapeuticArea {
+  Ageing,
+  BoneJointConnectiveTissue,
+  Cardiovascular,
+  Dermatology,
+  Endocrinology,
+  Gastroenterology,
+  Haematology,
+  Immunology,
+  InfectiousDiseaseOrVaccines,
+  Inflammation,
+  LiverDisease,
+  MentalHealth,
+  Metabolism,
+  Neurology,
+  Oncology,
+  Ophthalmology,
+  OralHealth,
+  Orthopaedics,
+  Otolaryngology,
+  Paediatrics,
+  Pulmonology,
+  Reproduction,
+  RareDiseases,
+  RegenerativeMedicine,
+  Respiratory,
+  UrologyRenalUrogenital,
+  WomensHealth,
+}
